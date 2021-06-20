7488b8cb856219a2b9143849ab794c19d1526138
abiyyumufti <akang.mufti@gmail.com>
2021-06-20 13:07:50 +0200

    using plausibilty in a function

--- checkPlausibilityBasiskoorsystem.m
+++ checkPlausibilityBasiskoorsystem.m
@@ -1,7 +1,17 @@
-function [outputArg1,outputArg2] = checkPlausibilityBasiskoorsystem(Basiskoorsystem)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = Basiskoorsystem;
-outputArg2 = Basiskoorsystem;
+function plausible = checkPlausibilityBasiskoorsystem(Basiskoorsystem)
+    [r, c] = size(Basiskoorsystem)
+    if r == 1
+        plausible = 1;
+        return;
+    end
+    
+    for i=1: length(Basiskoorsystem)-1
+        if Basiskoorsystem(i,1) == Basiskoorsystem((i+1),1)  || Basiskoorsystem(i,2) == Basiskoorsystem((i+1),2)  || Basiskoorsystem(i,3) == Basiskoorsystem((i+1),3) % Überprüfung der Koordinaten Link i und Link i+1 
+            plausible=1;
+        else
+            plausible=0; % Plausibilität ist nicht gegeben (error==1)
+            return; % Schleifenabbruch
+        end
+    end
 end
 
--- checkPlausibilityQuaternion.m
+++ checkPlausibilityQuaternion.m
@@ -1,7 +1,34 @@
-function [outputArg1,outputArg2] = checkPlausibilityQuaternion(quaternionZeros,quaternionVektors)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = quaternionZeros;
-outputArg2 = quaternionVektors;
+function [plausible, msg] = checkPlausibilityQuaternion(quaternionZeros,quaternionVektors)
+    for i=1: length(quaternionZeros)
+        if quaternionVektors(i,1) <= 1  && quaternionVektors(i,2) <= 1   && quaternionVektors(i,3) <= 1  % Überprüfung der der Quaternionen q1, q2, q3 <=1
+        else
+            msg = 'Quaternion muss kleiner als 1!\n - Bitte Eingabe der Quaternionen überprüfen';
+            plausible=0; % Plausibilität ist nicht gegeben (error_quaternionen==1)
+            return;
+        end
+    end
+    
+    for i=1: length(quaternionZeros)
+        if quaternionZeros(i) <= 1 % Überprüfung der Quaternion q0 <=1
+        else
+            msg = 'Quaternion muss kleiner als 1!\n - Bitte Eingabe der Quaternionen überprüfen';
+            plausible=0; % Plausibilität ist nicht gegeben (error_quaternionen==1)
+            return; % Schleifenabbruch
+        end
+    end
+    
+    for i=1: length(quaternionZeros) 
+
+       AbbruchKrit = quaternionZeros(i)^2+quaternionVektors(i,1)^2+quaternionVektors(i,2)^2+quaternionVektors(i,3)^2;
+
+       if AbbruchKrit == 0
+            msg = 'Bitte Eingabe der Quaternionen überprüfen!\nq0^2+q1^2+q2^2+q3^2 ist ungleich 1';
+            plausible=0; % Plausibilität ist nicht gegeben (error_quaternionen==1)
+            return; % Schleifenabbruch
+       end
+
+    end
+    msg = "ok";
+    plausible = 1;
 end
 
