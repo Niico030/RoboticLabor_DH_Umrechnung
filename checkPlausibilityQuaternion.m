397aac379b5524f2082b23c61501597c47ee9e63
abiyyumufti <akang.mufti@gmail.com>
2021-06-28 10:00:53 +0200

    using abs for check quaternion <= 1

--- checkPlausibilityQuaternion.m
+++ checkPlausibilityQuaternion.m
@@ -1,19 +1,20 @@
 function [plausible, msg] = checkPlausibilityQuaternion(quaternionZeros,quaternionVektors)
 % Überprüfung Plausibilität Quaternion:
-% 1) q0, q1, q2, q3 müssen <= 1 sein
+% 1) betrag von q0, q1, q2, q3 müssen <= 1 sein
 % 2) q0^2+q1^2+q2^2+q3^2 muss = 1 ergeben
 
     for i=1: length(quaternionZeros)
-        if quaternionVektors(i,1) <= 1  && quaternionVektors(i,2) <= 1   && quaternionVektors(i,3) <= 1  % Überprüfung der der Quaternionen q1, q2, q3 <=1
+        % Überprüfung der der Quaternionen |q1|, |q2|, |q3| <=1
+        if abs(quaternionVektors(i,1)) <= 1  && abs(quaternionVektors(i,2)) <= 1   && abs(quaternionVektors(i,3)) <= 1
         else
-            msg = 'Quaternion muss kleiner als 1!\n - Bitte Eingabe der Quaternionen überprüfen';
+            msg = 'Betrag der Quaternion muss kleiner als 1!\n - Bitte Eingabe der Quaternionen überprüfen';
             plausible=0; % Plausibilität ist nicht gegeben (error_quaternionen==1)
             return;
         end
     end
     
     for i=1: length(quaternionZeros)
-        if quaternionZeros(i) <= 1 % Überprüfung der Quaternion q0 <=1
+        if quaternionZeros(i) <= 1 % Überprüfung der Quaternion |q0| <=1
         else
             msg = 'Quaternion muss kleiner als 1!\n - Bitte Eingabe der Quaternionen überprüfen';
             plausible=0; % Plausibilität ist nicht gegeben (error_quaternionen==1)
@@ -22,7 +23,8 @@ function [plausible, msg] = checkPlausibilityQuaternion(quaternionZeros,quaterni
     end
     
     for i=1: length(quaternionZeros) 
-       AbbruchKrit = (quaternionZeros(i)^2) + (quaternionVektors(i,1)^2) + (quaternionVektors(i,2)^2) + (quaternionVektors(i,3)^2)
+       AbbruchKrit = (quaternionZeros(i)^2) + (quaternionVektors(i,1)^2) + (quaternionVektors(i,2)^2) + (quaternionVektors(i,3)^2);
+       % Mit toleranz von +/- 0.02
        if AbbruchKrit <= 1.02 && AbbruchKrit >= 0.98
        else
             msg = 'Bitte Eingabe der Quaternionen überprüfen!\nq0^2+q1^2+q2^2+q3^2 ist ungleich 1';
